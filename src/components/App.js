6d7a14c1fce367c9b3075f4ff672e7d75a0f4889
Ana Mongui <anamariamongui@gmail.com>
2020-09-19 04:50:47 -0500

    Cambios finales

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Home from '../pages/Home';
 import Badges from '../pages/Badges';
-import BadgeNew from '../pages/BagdeNew';
+import BadgeNew from '../pages/BadgeNew';
+import BadgeDetails from '../pages/BadgeDetailsContainer';
+import BadgeEdit from '../pages/BadgeEdit';
 import NotFound from '../pages/NotFound';
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
+          <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
